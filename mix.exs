a6bca25f74f24755477d6a8ac7a40dcd28228951
tunamsyar <tun.amsyar@gmail.com>
2021-06-02 00:07:37 +0800

    Added exdocs

--- mix.exs
+++ mix.exs
@@ -44,6 +44,7 @@ defmodule Blog.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false},
       {:plug_cowboy, "~> 2.0"}
     ]
   end
